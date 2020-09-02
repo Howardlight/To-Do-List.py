2676e12e0b078bf7780fa8c9d7f16871c053a460
Ciel/howard <67962926+Howardlight@users.noreply.github.com>
2020-09-02 21:39:38 +0300

    First commit

---
+++ Todo.py
@@ -0,0 +1,85 @@
+#! python3
+"""To Do list"""
+
+
+
+import sys
+
+# make a list or dictionnary that contains the to do list
+# find a way to save the to do list
+# find a way to display the to do list
+# find a way to add and delete items from the to do list
+
+ToDolist = []
+numberoftodos = 0
+
+# argnumber = len(sys.argv)
+# argparsed = str(sys.argv)
+# argslengh = len(sys.argv)
+args = sys.argv
+
+
+print('Welcome to To Do list! \nPlease select an argument. \n 1- add \n 2- remove \n 3- exit \n 4- show')
+while True:
+    inputtedCommand = input('>>')
+
+
+    if inputtedCommand == 'add':
+        newTodo = input('Please input your new todo >>')
+        ToDolist.append(newTodo)
+        print(f'Added "{newTodo}" to your Todo list!')
+
+
+    if inputtedCommand == 'remove':
+
+        if len(ToDolist) <= 0:
+            print('Your to do list is empty. \n')
+
+        else:
+            while True:
+
+                tododelete = input(f"Please select which todo you want to remove \n{str(ToDolist)} \n>>")
+
+                if tododelete > len(ToDolist):
+                    print('That todo does not exist!')
+
+                else:
+                    for i in range(1: ToDolist):
+                        
+
+
+
+    if inputtedCommand == 'exit':
+            print('exitting... \n')
+            break
+
+
+    if inputtedCommand == 'show':
+        for i in range(len(ToDolist)):
+            numberoftodos += 1
+            print(f"{numberoftodos}- {ToDolist[i]}")
+    
+
+    # TO BE ADDED #
+    #else:
+        #print("I'm sorry, i dont understand... \n")
+
+#print(" argument number: " + str(argnumber) + " arguments")
+#print('arguments detected: ' + argparsed)
+
+
+#if argslengh <= 1:
+    #print('Welcome to To Do list dot py! \n Please select an argument.')
+
+#if argslengh >= 1:
+   #rint(sys.argv[1])
+
+
+# find a way to add input data
+#if sys.argv[1] == 'add':
+    #if not sys.argv[2]:
+        #inputdetected = input('Please input the to do you want to add:')
+        #inputdetected
+    #print(f"Input detected : {inputdetected}." )
+#else:
+    #print('')
